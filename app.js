c87f6bd19005c53997692d45cfb30b8c34011a36
Adesoji Daniel <sojidaniels@gmail.com>
2025-01-30 20:14:55 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const multer = require('multer')
+const cloudinary = require('./integrations/cloudinary')
+const fs = require('fs')
+
+
+const upload = multer({ dest: 'uploads' })
+
+const port = 4000
+
+const app = express()
+
+
+app.post('/upload', upload.single('avatar'), async (req, res) => {
+    try {
+        console.log(req.file)
+
+        const response = await cloudinary.uploader.upload(req.file.path)
+
+
+        // delete the file after upload
+        fs.unlink(req.file.path, () => {
+            console.log('file deleted successfully')
+        })
+
+        return res.json({
+            message: 'successful upload!',
+            response
+        })
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            message: 'failed to upload file'
+        })
+    }
+})
+
+
+app.listen(port, () => {
+    console.log(`app is listening on port ${port}`)
+})
---
+++ integrations/cloudinary.js
@@ -0,0 +1,11 @@
+const cloudinary = require('cloudinary').v2
+
+
+cloudinary.config({ 
+    cloud_name: 'dt40xxwe4', 
+    api_key: '992659252339482', 
+    api_secret: 'jfRQrcDoOj4S5wmJD6HygtSjcgQ' // Click 'View API Keys' above to copy your API secret
+});
+
+module.exports = cloudinary;
+
